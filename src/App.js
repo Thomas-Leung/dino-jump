62f39366550e76b72497948f8bb3965ad486d133
Thomas-Leung <32366586+Thomas-Leung@users.noreply.github.com>
2022-11-14 19:22:20 -0500

    setup camera

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
+// import * as tf from '@tensorflow/tfjs';
+// import * as posenet from '@tensorflow-models/posenet';
 import './App.css';
 
 function App() {
+  // camera: {targetFPS: 60, sizeOption: '640 X 480'},
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const videoWidth = 640;
+  const videoHeight = 480;
+
+  useEffect(() => {
+    setupCamera();
+  }, [videoRef]);
+
+  const setupCamera = async () => {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      throw new Error(
+        'Browser API navigator.mediaDevices.getUserMedia not available'
+      );
+    }
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        facingMode: 'user',
+        width: videoWidth,
+        height: videoHeight,
+      },
+    });
+
+    let video = videoRef.current;
+    video.srcObject = stream;
+    video.play();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div>
+            <h1>hi</h1>
+            <video ref={videoRef} />
+          </div>
       </header>
     </div>
   );
